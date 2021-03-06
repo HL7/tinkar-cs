a4ae4936e3c12eae84755f921629fdd4cb0ebd30
Gjoll <KurtA@Gjoll.com>
2021-03-06 15:29:57 -0600

    Tests for guid equivelance.

--- Projects/Tinkar.XUnitTests/FieldCompareTests.cs
+++ Projects/Tinkar.XUnitTests/FieldCompareTests.cs
@@ -1,9 +1,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using Xunit;
 using Assert = Xunit.Assert;
 
@@ -11,6 +14,133 @@ namespace Tinkar.XUnitTests
 {
     public class FieldCompareTests
     {
+        Int32 CompareTo(Int64 a, Int64 b, Int64 c, Int64 d)
+        {
+            Int32 cmpVal = a.CompareTo(c);
+            if (cmpVal != 0)
+                return cmpVal;
+            return b.CompareTo(d);
+        }
+
+        bool IsEqualD(Int64[] aSet, Int64[] bSet)
+        {
+            switch (bSet.Length)
+            {
+                case 2:
+                    Int32 cmpVal = aSet[0].CompareTo(bSet[0]);
+                    if (cmpVal == 0)
+                        cmpVal = aSet[1].CompareTo(bSet[1]);
+                    return cmpVal == 0;
+                default:
+                    throw new Exception("");
+            }
+        }
+
+        bool IsEqualE(Int64[] aSet, Int64[] bSet)
+        {
+            if (bSet.Length == 2)
+            {
+                Int32 cmpVal = aSet[0].CompareTo(bSet[0]);
+                if (cmpVal == 0)
+                    cmpVal = aSet[1].CompareTo(bSet[1]);
+                return cmpVal == 0;
+            }
+            else
+                throw new NotImplementedException();
+        }
+
+        bool IsEqualF(Int64[] aSet, Int64[] bSet)
+        {
+            Int32 j = 0;
+            Int32 k = 0;
+            while (true)
+            {
+                Int32 cmpVal = aSet[j].CompareTo(bSet[k]);
+                if (cmpVal == 0)
+                    cmpVal = aSet[j + 1].CompareTo(bSet[k + 1]);
+                if (cmpVal < 0)
+                {
+                    j += 2;
+                    if (j >= aSet.Length)
+                        return false;
+                }
+                else if (cmpVal > 0)
+                {
+                    k += 2;
+                    if (k >= bSet.Length)
+                        return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        [Fact]
+        public void SpeedIntersectTest()
+        {
+            const Int32 iterations = 0x7ffff;
+            Guid a = new Guid(2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+            Guid b = new Guid(3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+            StringBuilder sb = new StringBuilder();
+
+
+            byte[] ab = a.ToByteArray();
+            long al1 = BitConverter.ToInt64(ab, 0);
+            long al2 = BitConverter.ToInt64(ab, 8);
+
+            byte[] bb = b.ToByteArray();
+            long bl1 = BitConverter.ToInt64(bb, 0);
+            long bl2 = BitConverter.ToInt64(bb, 8);
+
+
+            {
+                DateTime start = DateTime.Now;
+                for (Int32 i = 0; i < iterations; i++)
+                    CompareTo(al1, al2, bl1, bl2);
+                TimeSpan time = DateTime.Now - start;
+
+                sb.AppendLine($"Baseline Int64 comparisons {time}");
+            }
+
+            {
+                Int64[] aSet = new Int64[] { al1, al2 };
+                Int64[] bSet = new Int64[] { bl1, bl2 };
+                DateTime start = DateTime.Now;
+                for (Int32 i = 0; i < iterations; i++)
+                    IsEqualD(aSet, bSet);
+                TimeSpan time = DateTime.Now - start;
+
+                sb.AppendLine($"Use Switch. {time}");
+            }
+
+            {
+                Int64[] aSet = new Int64[] { al1, al2 };
+                Int64[] bSet = new Int64[] { bl1, bl2 };
+                DateTime start = DateTime.Now;
+                for (Int32 i = 0; i < iterations; i++)
+                    IsEqualE(aSet, bSet);
+                TimeSpan time = DateTime.Now - start;
+
+                sb.AppendLine($"Use if statements {time}");
+            }
+            {
+                Int64[] aSet = new Int64[] { al1, al2 };
+                Int64[] bSet = new Int64[] { bl1, bl2 };
+                DateTime start = DateTime.Now;
+
+                for (Int32 i = 0; i < iterations; i++)
+                    IsEqualF(aSet, bSet);
+                TimeSpan time = DateTime.Now - start;
+
+                sb.AppendLine($"Use array indices {time}");
+            }
+
+            File.WriteAllText(@"c:\Temp\f.txt", sb.ToString());
+        }
+
         [DoNotParallelize]
         [Fact]
         public void FieldCompareTest()
@@ -61,7 +191,7 @@ namespace Tinkar.XUnitTests
                     Misc.CreateConceptDTO
                     with
                     {
-                        PublicId = new PublicId(Misc.g2, Misc.g2, Misc.g3, Misc.g4 )
+                        PublicId = new PublicId(Misc.g2, Misc.g2, Misc.g3, Misc.g4)
                     }));
             }
             {
@@ -72,7 +202,7 @@ namespace Tinkar.XUnitTests
                     Misc.CreatePatternForSemanticChronologyDTO
                 with
                     {
-                        PublicId = new PublicId(Misc.g2, Misc.g2, Misc.g3, Misc.g4 )
+                        PublicId = new PublicId(Misc.g2, Misc.g2, Misc.g3, Misc.g4)
                     }));
             }
             {
@@ -84,7 +214,7 @@ namespace Tinkar.XUnitTests
                     Misc.CreatePatternForSemanticDTO
                 with
                     {
-                        PublicId = new PublicId(Misc.g2, Misc.g2, Misc.g3, Misc.g4) 
+                        PublicId = new PublicId(Misc.g2, Misc.g2, Misc.g3, Misc.g4)
                     }));
             }
             {
